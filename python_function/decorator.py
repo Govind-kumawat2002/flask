ecd17c5c592df530238a42160fa52bf5f95ab312
Govind-kumawat2002 <kumawatritik54@gmail.com>
2024-09-23 09:43:25 +0530

    decorator

---
+++ python_function/decorator.py
@@ -0,0 +1,10 @@
+def update(fx):
+    def mfx():
+        print("good morning baby")
+    fx()
+    print("baby how are you ")
+    return mfx
+@update
+def hello():
+    print("hello hiii baby")
+hello()
