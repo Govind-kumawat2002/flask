96e85deadd33d7905a2a025a56ba641fdb7a15c1
Govind-kumawat2002 <kumawatritik54@gmail.com>
2024-09-28 16:44:30 +0530

    flask oroject

---
+++ project1/main.py
@@ -0,0 +1,28 @@
+from flask import Flask ,render_template,request
+app=Flask(__name__)
+
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+@app.route('/predict',methods = ['GET','POST'])
+def predict():
+    if request.method =='POST':
+        user=request.form['username']
+        email=request.form['email']
+        password = request.form['password']
+
+        LST = ["username:",user,"user_email:",email,"user_passwor:",password]
+        print(LST)
+    
+    return render_template('form.html')
+    
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug= True)
