e03f3c05fb52e66b01cbab402331f7e438caa9e5
Govind-kumawat2002 <kumawatritik54@gmail.com>
2024-10-01 11:05:17 +0530

    sql with python

--- test/test.py
+++ test/test.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask,render_template,request
 app=Flask(__name__)
 
-# @app.route('/')
+import mysql.connector as conn
+server=conn.connect(host = '13.60.207.151',
+             user='govind',
+             password ='govind12345',
+              database= 'boss' )
+
+
 @app.route('/')
-def hello(name=None):
-    return render_template('basic.html', name=name)
-@app.route('/predict',methods =['GET','POST'])
-def predict():
+def home():
     return render_template('basic.html')
 
+@app.route('/predict',methods = ['POST','GET'])
+def predict():
+    if request.method=="POST":
+        user1 =request.form['username']
+        password1 = request.form['password']
+        a=server.cursor()
 
-if __name__=='__main__':
+
+        query = 'select *from logine'
+        query= 'insert into logine(user,password)values(%s,%s)'
+        values= (user1,password1)
+        a.execute(query,values)
+        server.commit()
+       
+
+        return render_template('basic.html')
+
+
+if __name__=="__main__":
     app.run(debug=True)
