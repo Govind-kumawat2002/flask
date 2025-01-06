144381ec2ef775293e60d04cee6a637e1d876468
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-01-07 01:38:57 +0530

    flask with jinja2 templates

--- flask_by_krishnayak/basic.py
+++ flask_by_krishnayak/basic.py
@@ -17,31 +17,48 @@
 
 # building url diamecally 
 
-from flask import Flask ,redirect,url_for
+# from flask import Flask ,redirect,url_for,render_template
+# app=Flask(__name__)
+
+
+
+
+
+
+
+# @app.route('/succes/<int:score>')  ### if we are pass the url data then we are to this method  int is deternime the dtype 
+# def succes(score):
+#     return "the persom has passed and the marks is "+str(score)  # binding function 
+# @app.route('/fail/<int:score>')
+# def fail(score):
+#     return "the persom has fail and the marks is "+str(score)  # binding function
+# # result checker  
+# @app.route('/result/<int:marks>')
+# def myresult(marks):
+#     result = ""
+#     if marks<50:
+#         result = 'fail'
+#     else :
+#         result = 'succes'
+#     # return result
+#     return redirect(url_for(result,score=marks))  #  that is used to call the direct other route 
+
+# @app.route('')
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+
+# @app.route('/submit',method=['POST',"GET"])
+# def submit():
+    # if method =='POST':
+
+from flask import Flask ,redirect,url_for,render_template
 app=Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return "Welcome to my repositry"
-@app.route('/succes/<int:score>')
-def succes(score):
-    return "the persom has passed and the marks is "+str(score)  # binding function 
-@app.route('/fail/<int:score>')
-def fail(score):
-    return "the persom has fail and the marks is "+str(score)  # binding function
-# result checker  
-@app.route('/result/<int:marks>')
-def myresult(marks):
-    result = ""
-    if marks<50:
-        result = 'fail'
-    else :
-        result = 'succes'
-    # return result
-    return redirect(url_for(result,score=marks))
-
 
 
 if __name__=="__main__":
     app.run(debug= True)
+@app.route('/')
+def home():
+    return render_template('index.html')
