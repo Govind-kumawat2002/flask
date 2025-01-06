738a0d4aada30bf14550cd230acd43679a541ac7
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-01-06 23:51:12 +0530

    hello flask

---
+++ flask_by_krishnayak/basic.py
@@ -0,0 +1,47 @@
+# from flask import Flask
+# app=Flask(__name__)  # wsgi(web server The full form of WSGI is Web Server Gateway Interface.
+
+# # WSGI is a specification that defines how web servers communicate with web applications in Python.)
+# @app.route('/')
+# def home():
+#     return "Welcome to my repositry"
+# @app.route('/member')
+# def member():
+#     return "Welcome to my repositry member"  # binding function 
+
+
+
+# if __name__=="__main__":
+#     app.run(debug= True)
+
+
+# building url diamecally 
+
+from flask import Flask ,redirect,url_for
+app=Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Welcome to my repositry"
+@app.route('/succes/<int:score>')
+def succes(score):
+    return "the persom has passed and the marks is "+str(score)  # binding function 
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "the persom has fail and the marks is "+str(score)  # binding function
+# result checker  
+@app.route('/result/<int:marks>')
+def myresult(marks):
+    result = ""
+    if marks<50:
+        result = 'fail'
+    else :
+        result = 'succes'
+    # return result
+    return redirect(url_for(result,score=marks))
+
+
+
+if __name__=="__main__":
+    app.run(debug= True)
